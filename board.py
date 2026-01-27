60a5baafb2b83969f8f005f61fb2102a0202aa7a
Boat <bkotchasarn@gmail.com>
2026-01-27 13:25:59 +0700

    Update Board's rules

--- board.py
+++ board.py
@@ -1,5 +1,7 @@
 from actions import Action, MovePawn, PlaceFence
 
+from igraph import Graph
+
 # TODO: Saving and initializing from a board position
 class Board:
     def __init__(self, rows=9, cols=9):
@@ -7,11 +9,22 @@ class Board:
         self.is_terminal = False
         self.rows = rows
         self.cols = cols
-        self.p1_pawn = (1, 5)
-        self.p2_pawn = (9, 5)
+        self.p1_pawn = (1, (cols + 1) /  2)
+        self.p2_pawn = (rows, (cols + 1) / 2)
         self.p1_fences = []
         self.p2_fences = []
         self.fences = []
+        self.valid_fence_placements = {
+            (x, y, True) for x in range(1, cols) for y in range(1, rows)
+        } | {
+            (x, y, False) for x in range(1, cols) for y in range(1, rows)
+        }
+
+        # Initializing the board's graph representation (to disallow disconnecting fence placement)
+        vertices = [(x, y) for x in range(1, cols + 1) for y in range(1, rows + 1)]
+        vertex_id = {v: i for i, v in enumerate(vertices)}
+        edges = []  # TODO
+        self.graph = Graph(edges=edges, directed=False)
 
         # TODO: Should each player's number of remaining fences be tracked here?
     
@@ -29,30 +42,72 @@ class Board:
         
         self.fences.append((x, y, direction))
 
+        blocked_placements = {(x, y, direction), (x, y, ~direction)}
+        if direction:
+            blocked_placements |= {(x - 1, y, direction), (x + 1, y, direction)}
+        else:
+            blocked_placements |= {(x, y - 1, direction), (x, y + 1, direction)}
+
+        self.valid_fence_placements -= blocked_placements
+
     def get_valid_pawn_moves(self) -> set[tuple[int, int]]:
         val_moves = set()
         if self.curr_player == 1:
             x, y = self.p1_pawn
+            x_opp, y_opp = self.p2_pawn
+        elif self.curr_player == 2:
+            x, y = self.p2_pawn
+            x_opp, y_opp = self.p1_pawn
 
-            # 1. Move-up
-            if not (((x - 1, y, True) in self.fences) or ((x, y, True) in self.fences)):
-                val_moves.add((x, y + 1))
+        # Potentially allowing move-up/down/left/right respectively
+        if not (((x - 1, y, True) in self.fences) 
+                or ((x, y, True) in self.fences)):
+            val_moves.add((x, y + 1))
+        
+        if not (((x - 1, y - 1) in self.fences) 
+                or ((x, y - 1) in self.fences)):
+            val_moves.add((x, y - 1))
+
+        if not (((x - 1, y) in self.fences) 
+                or ((x - 1, y - 1) in self.fences)):
+            val_moves.add((x - 1, y))
+        
+        if not (((x, y) in self.fences) 
+                or ((x, y - 1) in self.fences)):
+            val_moves.add((x + 1, y))
+        
+        if (x_opp, y_opp) in val_moves:
+            val_moves.dicard((x_opp, y_opp))
+
+            # Potentially allowing move-up/down/left/right from opponent respectively
+            if not (((x_opp - 1, y_opp, True) in self.fences) 
+                    or ((x_opp, y_opp, True) in self.fences)):
+                val_moves.add((x_opp, y_opp + 1))
             
-            # 2. Move-down
-            if not (((x - 1, y - 1) in self.fences) or ((x, y - 1) in self.fences)):
-                val_moves.add((x, y - 1))
+            if not (((x_opp - 1, y_opp - 1) in self.fences) 
+                    or ((x_opp, y_opp - 1) in self.fences)):
+                val_moves.add((x_opp, y_opp - 1))
+
+            if not (((x_opp - 1, y_opp) in self.fences) 
+                    or ((x_opp - 1, y_opp - 1) in self.fences)):
+                val_moves.add((x_opp - 1, y_opp))
             
-            # TODO: Move-left, move-right
-            # TODO: Consider blockage by player and jumping moves.
+            if not (((x_opp, y_opp) in self.fences) 
+                    or ((x_opp, y_opp - 1) in self.fences)):
+                val_moves.add((x_opp + 1, y_opp))
+        
+
+        val_moves = {move for move in val_moves 
+                     if (move[0] > 0 and move[0] <= self.cols)}
 
         return val_moves
     
     def get_valid_fence_placements(self) -> set[tuple[int, int, bool]]:
-        val_placements = set()
         # TODO: Get non-blocking fence placements.
+
         # TODO: Ensure the board is not totally divided.
         
-        return val_placements
+        pass
 
     def update(self, action: Action):
         if isinstance(action, MovePawn):
