ae18dcb947617be544738b463dac07f10f108d4f
Boat <bkotchasarn@gmail.com>
2026-01-29 10:48:56 +0700

    Touching up board rules. Only one for fence placement left

--- board.py
+++ board.py
@@ -23,10 +23,18 @@ class Board:
         # Initializing the board's graph representation (to disallow disconnecting fence placement)
         vertices = [(x, y) for x in range(1, cols + 1) for y in range(1, rows + 1)]
         vertex_id = {v: i for i, v in enumerate(vertices)}
-        edges = []  # TODO
+
+        edges = [(vertex_id((i, j)), vertex_id((i + 1, j))) 
+                 for i in range(1, cols) for j in range(1, rows + 1)]
+        
+        edges += [(vertex_id((i, j)), vertex_id((i, j + 1))) 
+                 for i in range(1, cols + 1) for j in range(1, rows)]
+
         self.graph = Graph(edges=edges, directed=False)
+        self.graph.vs["name"] = vertices
 
         # TODO: Should each player's number of remaining fences be tracked here?
+
     
     def move_pawn(self, x, y):
         if self.curr_player == 1:
@@ -49,6 +57,14 @@ class Board:
             blocked_placements |= {(x, y - 1, direction), (x, y + 1, direction)}
 
         self.valid_fence_placements -= blocked_placements
+
+        if direction:
+            self.graph.delete_edges([
+                (self.graph.vs.find(name=(x, y)),
+                 self.graph.vs.find(name=(x, y + 1))),
+                (self.graph.vs.find(name=(x + 1, y)),
+                 self.graph.vs.find(name=(x + 1, y + 1)))
+            ])
 
     def get_valid_pawn_moves(self) -> set[tuple[int, int]]:
         val_moves = set()
@@ -103,10 +119,31 @@ class Board:
         return val_moves
     
     def get_valid_fence_placements(self) -> set[tuple[int, int, bool]]:
-        # TODO: Get non-blocking fence placements.
 
         # TODO: Ensure the board is not totally divided.
+        # Use igraph all_minimal_st_separators to check each separator of size <= 2
         
+        seps = self.graph.all_minimal_st_separators()
+        two_vertex_seps = []
+        one_vertex_seps = []
+        for sep in seps:  # sep is a set of vertices
+            if len(sep) == 2:
+                two_vertex_seps.append(list(map(Board._vertex_id_to_name, sep)))
+            elif len(sep) == 1:
+                one_vertex_seps.append(Board._vertex_id_to_name(sep.pop()))
+        
+        two_vertex_seps = [sep for sep in two_vertex_seps 
+                           if Board._are_adjacent_cells(sep[0], sep[1])]
+        
+        # Almost done. This is getting complicated. Maybe check one by one instead.
+        
+
+    @staticmethod 
+    def _are_adjacent_cells(coord_1, coord_2) -> bool:
+        pass
+
+    @staticmethod
+    def _vertex_id_to_name(vertex_id: int) -> tuple[int, int]:
         pass
 
     def update(self, action: Action):
